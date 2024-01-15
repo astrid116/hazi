adb2108863f5608d32a1115fda77d42d00630ebf
astrid116 <jaffadani@gmail.com>
2024-01-15 21:33:29 +0100

    kész házi

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace hazi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int sum = 0;
+            int avg;
+
+            var num = new int[10];
+            Console.WriteLine("Adj meg 10 számot!");
+            for (int i = 0; i < num.Length; i++)
+            {
+                input = Console.ReadLine();
+                for (int j = 0; j < num.Length; j++)
+                {
+                    if (num[j] == int.Parse(input))
+                    {
+                        Console.WriteLine("Egy számot csak egyszer adhatsz meg.");
+                        i--;
+                        break;
+                    }
+                }
+                num[i] = int.Parse(input);
+            }
+            for (int i = 0; i < num.Length; i++)
+            {
+                sum += num[i];
+            }
+            avg = sum / num.Length;
+            Console.WriteLine("A számok összege: {0}", sum);
+            Console.WriteLine("A számok átlaga: {0}", avg);
+            Console.ReadKey();
+        }
+    }
+}
